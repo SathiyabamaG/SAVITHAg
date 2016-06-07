77e97fa44fa838b6171067ae7d639384985c30b1
SathiyabamaG <savitha940@gmail.com>
2016-06-07 12:33:34 +0530

    Create conson.c

---
+++ conson.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int main()
+{
+  char ch='g';
+  switch(ch)
+  {
+  case 'a':
+  printf("vowel");
+   break;
+  case 'e':
+  printf("vowel");
+   break;
+  case 'i':
+  printf("vowel");
+   break;
+  case 'o':
+  printf("vowel");
+   break;
+  case 'u':
+  printf("vowel");
+   break;
+  case 'A':
+  printf("vowel");
+   break;
+  case 'E':
+  printf("vowel");
+   break;
+  case 'I':
+  printf("vowel");
+  case 'O':
+  printf("vowel");
+   break;
+  case 'U':
+  printf("vowel");
+   break;
+  default:
+  printf("consonaunt");
+  break;
+  }
+  }
+  
